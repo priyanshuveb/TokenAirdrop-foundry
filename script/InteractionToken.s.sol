687dd19e9ba950b6d0d8b4e0e1c5a15e68b009f9
priyanshuveb <priyanshubindal@gmail.com>
2024-02-02 15:58:10 +0530

    interaction token file name change

---
+++ script/InteractionToken.s.sol
@@ -0,0 +1,64 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+import {Script, console} from "forge-std/Script.sol";
+import {TestToken} from "../src/TestToken.sol";
+
+contract TokenInstance {
+    TestToken testToken = TestToken(0x94B46C7fE53Bac1Bbb94FA980975F53Bc7DF5F8D);
+}
+
+contract GetBalance is Script, TokenInstance {
+    function run() public view returns (uint256) {
+        return getBalance(msg.sender);
+    }
+
+    function getBalance(address user) public view returns (uint256) {
+        uint256 balance = testToken.balanceOf(user);
+        return balance;
+    }
+}
+
+contract MintTokens is Script, TokenInstance {
+    uint256 amount = 1 * 10 ** 8;
+
+    function run() public {
+        mint();
+    }
+
+    function mint() internal {
+        vm.startBroadcast();
+        testToken.mint(amount);
+        vm.stopBroadcast();
+    }
+}
+
+contract BurnTokens is Script, TokenInstance {
+    uint256 amount = 1 * 10 ** 7;
+
+    function run() external {
+        burn();
+    }
+
+    function burn() internal {
+        vm.startBroadcast();
+        testToken.burn(amount);
+        vm.stopBroadcast();
+    }
+}
+
+contract Transfer is Script, TokenInstance {
+    address to = 0xC6ad6C00877a05a0ac1BBD456e31792c6b561F8D;
+    uint256 amount = 43 * 10 ** 2;
+
+    function run() external {
+        transfer();
+    }
+
+    function transfer() internal {
+        vm.startBroadcast();
+        testToken.transfer(to, amount);
+        vm.stopBroadcast();
+    }
+}
